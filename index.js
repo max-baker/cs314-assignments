d4bdacc0fb1335ccc225fa83eea63079321cb6e8
mbaker7@zagmail.gonzaga.edu <mbaker7@zagmail.gonzaga.edu>
2018-04-04 22:30:18 -0700

    FirstHalf

--- index.js
+++ index.js
@@ -1,60 +1,61 @@
-function getResponse(){
-	let response=confirm();
-	if(confirm==1){
-		alert("yes");
-	} else{
-		alert("no");
+'use strict';
+//Task 1
+function CharSwap(string){
+	if(string.length<2){
+		console.log("String must be longer than 0");
+	}else{
+		let last = string.length-1;
+		let newStr = string[last]+string.substr(1,last-1)+string[0];
+		return newStr;
 	}
 }
-
-function getResponseAlt(){
-	alert((confirm())?"yup":"nope");
-}
-
-function getNumberInput(){
-	let input=prompt("enter a number");
-	if(input==null){
-		alert("canceled");
-	} else if(input==""){
-		alert("you didn't enter anything");
-	} else if(isNaN(input)){
-		alert("Not a number");
-	} else{
-		alert("You did it");
+//Task 2
+function HiString(string){
+	if(string.length<2){
+		console.log("String must be longer than 0");
+	}else{
+		if(string.substr(0,2)=='Hi'){
+			return string;
+		}else{
+			return 'Hi'+string;
+		}
 	}
 }
-
-function MyObject(){
-	this.name="object";
-	this.id=21;
-	this.codeName=function(){return this.name+this.id;};
-}
-
-function makeObjectCopy(myObject){
-	let myCopy={};
-	for(let key in myObject){
-		myCopy[key] = myObject[key];
+//Task 3
+function ThreeCharsToFront(string){
+	if(string.length<4){
+		console.log("String must be longer than 2");
+	}else{
+		let last = string.length-1;
+		return string.substr(last-2,last)+string.substr(0,last-2);
 	}
-	myCopy.extra = "thisIsAdded";
-	console.log("copy",myCopy);
 }
-
-/*
-function stringCat(str1,str2,str3="default"){
-	return str1+str2+str3;
+//Task 4
+function StringsToSentence(){
+	let string = window.prompt();
+	let words = string.split(',');
+	if(words.length != 3){
+		console.log("please enter three words separated by commas");
+	}
+	alert('My favorite color is ' +words[0]+ ', '+words[1]+' are awesome, and I love '+string[2]+'!');
 }
-*/
-
-//let stringCat2 = function(str1,str2,str3="default"){return str1+str2+str3};
-
-let stringCat3 = (str1,str2,str3="default")=>str1+str2+str3;
-
-getResponse();
-getResponseAlt();
-getNumberInput();
-let test= new MyObject();
-console.log("original",test);
-makeObjectCopy(test);
-//console.log(stringCat("yeah","boy"));
-//console.log(stringCat2("yeah","buoy"));
-console.log(stringCat3("yea","buoy"));
+//Task 5
+function UpperOrLower(){
+	let string = window.prompt();
+	if(string.length<3){
+		return string.toUpperCase();
+	}else{
+		return string.substr(0,3).toLowerCase()+string.substr(3,string.length-1).toUpperCase();
+	}
+}
+//Task 6
+//Task 7
+//Task 8
+//Task 9
+//Task 10
+//Function Calls
+//console.log(CharSwap(window.prompt()));
+//console.log(HiString(window.prompt()));
+//console.log(ThreeCharsToFront(window.prompt()));
+//console.log(StringsToSentence());
+//console.log(UpperOrLower());
